3a2068705cd6c1c2a744d1adef665d852f796f67
Wiktor Sienkiewicz <wiktorsienkiewicz99@gmail.com>
2025-04-23 17:30:14 +0200

    Init
    Initial commit

---
+++ Log.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+//
+// Created by Wiktor Sienkiewicz on 23/04/2025.
+//
+void Log(const char* message) {
+    std::cout << message << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void Log(const char* message);
+
+int main() {
+
+    Log("message");
+
+    return 0;
+}
